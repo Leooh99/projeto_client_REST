77269d24c6723511b18373733a350712504ec966
Leonardo Andrade Heldt <godshadowkhan@gmail.com>
2023-05-19 20:38:32 -0300

    First commit added all the web code

---
+++ script.js
@@ -0,0 +1,19 @@
+function convertCurrency() {
+  var amount = document.getElementById('amount').value;
+  var fromCurrency = document.getElementById('from').value;
+  var toCurrency = document.getElementById('to').value;
+
+  // Substitua 'YOUR_API_KEY' pela sua chave de API válida
+  var apiKey = '3bb8a3a4adf5e592550a3f69';
+
+  var url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`;
+
+  axios.get(url)
+    .then(function(response) {
+      var result = response.data.conversion_result.toFixed(2);
+      document.getElementById('result').innerHTML = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
